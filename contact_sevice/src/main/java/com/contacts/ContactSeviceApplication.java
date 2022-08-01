f8c07345d928a77c9e637adb54ad4a9f6e7db23c
Muhammad Mukkarram <mukki0303@gmail.com>
2022-08-02 02:02:21 +0500

    initialize

---
+++ contact_sevice/src/main/java/com/contacts/ContactSeviceApplication.java
@@ -0,0 +1,13 @@
+package com.contacts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactSeviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactSeviceApplication.class, args);
+	}
+
+}
---
+++ contact_sevice/src/test/java/com/contacts/ContactSeviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contacts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactSeviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
